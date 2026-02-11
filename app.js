50a36f7072d1c1a8500eb093420715f8d48f0c02
Tofel-cyber <muchamadtofel88@gmail.com>
2026-02-11 21:08:52 +0700

    fix pi testnet login sandbox true

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
 function login() {
-  const btn = document.getElementById("loginBtn");
-
   if (!window.Pi) {
-    alert("❌ Harus dibuka lewat Pi Browser");
+    alert("❌ Buka lewat Pi Browser");
     return;
   }
 
-  btn.disabled = true;
+  console.log("🔑 Login dimulai");
 
   Pi.authenticate(
-    ["username", "payments"],
+    ["username"],
     (auth) => {
-      console.log("✅ Login sukses:", auth);
+      console.log("✅ Login sukses", auth);
 
       const user = auth.user;
 
       document.getElementById("status").innerText =
         "Assalamu'alaikum, " + user.username;
 
-      document.getElementById("userInfo").innerText =
-        "👤 " + user.username;
-
-      document.getElementById("userInfo").style.display = "block";
       document.getElementById("loginSection").style.display = "none";
       document.getElementById("modeSection").style.display = "block";
-
-      btn.disabled = false;
     },
     (err) => {
-      console.error("❌ Login gagal:", err);
-      alert("Login Pi dibatalkan / gagal");
-      btn.disabled = false;
+      console.error("❌ Login gagal", err);
+      alert("Login Pi dibatalkan");
     }
   );
 }
